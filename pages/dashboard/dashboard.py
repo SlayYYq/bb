5d0d772b83434d8ac11457147998947304d90f94
SlayYYq <90299364+SlayYYq@users.noreply.github.com>
2024-10-12 09:01:54 -0400

    Delete pages directory

--- pages/dashboard/dashboard.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from util import auth
-
-Dashboard = Blueprint("dashboard", __name__, template_folder="pages")
-
-@Dashboard.route("/dashboard", methods=["GET"])
-@auth.authenticated_required
-def dashboard():
-    return render_template("dashboard/dashboard.html")
